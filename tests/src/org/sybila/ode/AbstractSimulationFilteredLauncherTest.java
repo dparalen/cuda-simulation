cc54499d98e36abbf58ed101a385a2f0840a825c
dparalen <vetrisko@gmail.com>
2011-09-03 19:15:21 +0200

    constant update

--- tests/src/org/sybila/ode/AbstractSimulationFilteredLauncherTest.java
+++ tests/src/org/sybila/ode/AbstractSimulationFilteredLauncherTest.java
@@ -4,6 +4,6 @@ import org.sybila.ode.SimulationFilterGuards;
 
 public abstract class AbstractSimulationFilteredLauncherTest extends AbstractSimulationLauncherTest
 {
-	protected static final float[] GUARDS = {0.0f, 0.0f};
+	protected static final float[] GUARDS = {0.4f, 0.3f};
 	protected static final int[] GUARDINDEXES = {0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
 }
